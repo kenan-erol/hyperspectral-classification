490497685fa23c7babe5fd25ccf9427ea7550b3e
kenan-erol <kenan.erol@yale.edu>
2025-04-25 13:00:12 -0400

    create hyper preproc so that we dont spam sam during training

---
+++ bash/preproc_hyper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# pwd
+# cd $SLURM_SUBMIT_DIR
+# pwd
+
+export CUDA_VISIBLE_DEVICES=0
+
+python tools/preproc_patch.py \
+--n_batch 25 \
+--data_dir './data_processed/' \
+--label_file './labels.txt' \
+--num_patches_per_image 100 \
+--patch_size 224 \
+--train_split_ratio 0.8 \
+--encoder_type 'resnet18' \
+--num_channels 256 \
+--n_epoch 50 \
+--learning_rate 0.001 \
+--learning_rate_decay 0.5 \
+--learning_rate_period 10 \
+--checkpoint_path 'hyper_checkpoints/resnet/' \
+--device 'cuda'
