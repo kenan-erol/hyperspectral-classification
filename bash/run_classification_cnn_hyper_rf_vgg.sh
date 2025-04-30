48f971021df3de36024ab256df0847582d1d1f73
kenan-erol <kenan.erol@yale.edu>
2025-04-30 10:47:42 -0400

    init run hyper for rf

---
+++ bash/run_classification_cnn_hyper_rf_vgg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+python src/run_classification_hyper.py \
+  --n_batch 25 \
+  --data_dir './data_real_fake' \
+  --label_file './labels_real_fake.txt' \
+  --patch_size 224 \
+  --train_split_ratio 0.8 \
+  --encoder_type 'vggnet11' \
+  --num_channels 256 \
+  --checkpoint_path 'hyper_checkpoints/vgg_rf/model-37.pth' \
+  --device 'cuda'
