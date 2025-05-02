85540954d677d878168e26bda3f15e0730e721b7
Kenan Erol <kge7@login1.grace.ycrc.yale.edu>
2025-05-02 10:17:33 -0400

    rf running and trying 25 on class

--- bash/run_classification_cnn_hyper_rf_resnet.sh
+++ bash/run_classification_cnn_hyper_rf_resnet.sh
@@ -5,10 +5,9 @@ export CUDA_VISIBLE_DEVICES=0
 python src/run_classification_hyper.py \
   --n_batch 25 \
   --data_dir './data_real_fake' \
-  --label_file './labels_real_fake.txt' \
+  --label_file './labels_real_fake2.txt' \
   --patch_size 224 \
-  --train_split_ratio 0.8 \
   --encoder_type 'resnet18' \
   --num_channels 256 \
-  --checkpoint_path 'hyper_checkpoints/resnet_rf/model-36.pth' \
+  --checkpoint_path 'hyper_checkpoints/resnet_rf/model-20.pth' \
   --device 'cuda'
