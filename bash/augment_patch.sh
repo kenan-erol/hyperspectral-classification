6aa218c7a278cbc070c802b0dad1c80294ccd77b
Kenan Erol <kge7@login2.grace.ycrc.yale.edu>
2025-05-03 00:59:49 -0400

    weak aug results

--- bash/augment_patch.sh
+++ bash/augment_patch.sh
@@ -2,11 +2,16 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-python tools/augment_patches.py   --input_dir './data_processed_patch/patches/'   --output_dir './data_real_fake/fake'   --noise_std_dev 0.05 \ 
-  --apply_scaling \
-  --scale_factor_range 0.9 1.1 \
-  --apply_offset \
-  --offset_range -0.05 0.05 \ 
-  --seed 42 \
-  --visualize_count 9 \   
-  --visualize_dir ./data_fake/visualizations --input_label_filename './data_processed_patch/labels_patches.txt'
+python tools/augment_patches.py \
+	--input_dir './data_processed_patch/patches/' \
+	--output_dir './data_real_fake/fake' \
+	--noise_std_dev 0.05 \
+	--apply_scaling \
+	--scale_factor_range 0.9 1.1 \
+	--apply_offset \
+	--offset_range -0.05 0.05 \
+	--seed 42 \
+	--visualize_count 9 \
+	--visualize_dir ./data_fake/visualizations \
+	--input_label_filename './data_processed_patch/labels_patches.txt'
+
--- bash/compare_rf.sh
+++ bash/compare_rf.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python tools/compare_real_fake_patches.py \
-  --real_patch_path 'data_processed_patch/patches/Bromazolam 2025-01-20/Group/M0001/measurement_patch_38.npy' \
-  --fake_patch_path './data_fake/patches_augmented/Bromazolam 2025-01-20/Group/M0001/measurement_patch_38.npy' \
+  --real_patch_path 'data_real_fake/real/Tramadol 2025-01-14/Tramadol 2025-01-14/M0001/measurement_patch_8.npy' \
+  --fake_patch_path './data_real_fake/fake/patches_augmented/Tramadol 2025-01-14/Tramadol 2025-01-14/M0001/measurement_patch_8.npy' \
   --output_dir ./patch_comparisons \
-  --pixels 112,112 50,50
+  --pixels 112,112 0,0
